3306ca9841cc41f2e75ba3d94b01910cac0ae93c
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2024-02-02 18:52:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect } from 'react';
-
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
